78ec71c846643e95096a5222819151e6e2217c0d
Kittaru87 <sophbell87@gmail.com>
2020-08-12 17:14:43 +0100

    article wall is now in the center of the page

--- packages/beauty-portal-theme/src/components/ArticleWall/index.tsx
+++ packages/beauty-portal-theme/src/components/ArticleWall/index.tsx
@@ -1,6 +1,7 @@
 import React, { FunctionComponent } from 'react';
 import { Link } from 'gatsby';
 import Wall from '../Wall';
+import classNames from 'classnames';
 import { ArticleWallInterface } from './models';
 import { getSearchUrlWithTagsAndCategory } from '../../helpers/searchUrl';
 import { blockTypeDefaultSerializers } from '../../helpers/sanity';
@@ -29,41 +30,43 @@ const ArticleWall: FunctionComponent<ArticleWallInterface> = ({
   };
 
   return (
-    <div className={classes.wall}>
-      <div className={classes.sectionDivider}></div>
-      <div className={classes.sectionTitle}>
-        {headline && <h2 className={classes.sliderTitle}>{headline}</h2>}
-      </div>
-      <div className={classes.sectionDescription}>
-        {_rawTextBlockBody && (
-          <BlockContent
-            serializers={blockTypeDefaultSerializers}
-            blocks={_rawTextBlockBody}
-          />
-        )}
-      </div>
-      <Wall
-        type="tile"
-        slides={slides}
-        spaceBetween={30}
-        slidesPerView={4}
-        speed={700}
-        threshold={5}
-        lazy={true}
-        preloadImages={false}
-        freeMode={true}
-        watchSlidesVisibility={true}
-        breakpoints={breakpoints}
-      />
-      <div className={classes.articleNav}>
-        {searchCtaLabel && (
-          <Link
-            className={classes.sectionLink}
-            to={getSearchUrlWithTagsAndCategory(searchTags)}
-          >
-            {searchCtaLabel}
-          </Link>
-        )}
+    <div className={classNames('container', 'pad0')}>
+      <div className={classes.wall}>
+        <div className={classes.sectionDivider}></div>
+        <div className={classes.sectionTitle}>
+          {headline && <h2 className={classes.sliderTitle}>{headline}</h2>}
+        </div>
+        <div className={classes.sectionDescription}>
+          {_rawTextBlockBody && (
+            <BlockContent
+              serializers={blockTypeDefaultSerializers}
+              blocks={_rawTextBlockBody}
+            />
+          )}
+        </div>
+        <Wall
+          type="tile"
+          slides={slides}
+          spaceBetween={30}
+          slidesPerView={4}
+          speed={700}
+          threshold={5}
+          lazy={true}
+          preloadImages={false}
+          freeMode={true}
+          watchSlidesVisibility={true}
+          breakpoints={breakpoints}
+        />
+        <div className={classes.articleNav}>
+          {searchCtaLabel && (
+            <Link
+              className={classes.sectionLink}
+              to={getSearchUrlWithTagsAndCategory(searchTags)}
+            >
+              {searchCtaLabel}
+            </Link>
+          )}
+        </div>
       </div>
     </div>
   );
